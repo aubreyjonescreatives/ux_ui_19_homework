644f8b86ed9e365bd828c952b890d7c3ec4d2e5b
Aubrey Jones <aubrey_jones07@hotmail.com>
2024-03-27 18:07:29 -0600

    added navigation

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var a = 2; 
+let b = 4; 
+const c = 6; 
+
+console.log(a * b);
+
+console.log('Hello World!');
+
+console.log(a); 
+
+console.log(3); 
+
+
+$(document).ready(function(){
+    $("#homeLink").hover(function(){
+      $(this).css("background-color", "#cdcdcd");
+      $(this).css("margin", "20px");
+      }, function(){
+      $(this).css("background-color", "white");
+      $(this).css("margin", "0px");
+    });
+  });
+
+
+
+//   # is for ids
+// . is for classes 
